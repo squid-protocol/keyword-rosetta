2e54835d139ebe47ef8dc4dfa667a7b4e816ece1
Joe Esquibel <squid-protocol@users.noreply.github.com>
2026-09-04 19:41:03 -0400

    corpus(go): explicit api surface (gitgalaxy#2729)

--- data/go/a.go
+++ data/go/a.go
@@ -4,19 +4,19 @@ package main
 
 import "b"
 
-func probeGlobals(env int) int {
+func ProbeGlobals(env int) int {
     os.Getenv(env)
     os.Environ(env)
     return env
 }
 
-func probeTest(kit int) int {
+func ProbeTest(kit int) int {
     t.Run(kit)
     t.Run(kit)
     return kit
 }
 
-func probeSafety(value int) int {
+func ProbeSafety(value int) int {
     context.Context(value)
     context.Context(value)
     return value
--- data/go/c.go
+++ data/go/c.go
@@ -2,18 +2,18 @@
 // decoy: tidy remarks stay in prose and the work happens elsewhere
 package main
 
-func probeCleanup(conn int) int {
+func ProbeCleanup(conn int) int {
     Close(conn)
     Stop(conn)
     return conn
 }
 
-func probeDebt(level int) int {
+func ProbeDebt(level int) int {
     // HACK: shortcut kept deliberately for the rosetta corpus
     return level
 }
 
-func probeTodo(plan int) int {
+func ProbeTodo(plan int) int {
     // TODO: fill in the probe body later
     return plan
 }
